bd94d3bddf9c5b57d797182de21f50e8eb4a551b
kvandenbrande <kvandenbrande@gmail.com>
2017-03-13 09:51:29 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,32 +1,42 @@
-import MySQLdb
-#if not installed: sudo apt-get install python-mysqldb
+import MySQLdb #if not installed: sudo apt-get install python-mysqldb
+import json
+
+def DB_conn():
+
+    with open ('/home/pi/Desktop/welkomscherm_conf.json','r') as f:
+        config = json.load(f)
+
+    host= config['DB']['host']
+    user= config['DB']['user']
+    passwd= config['DB']['passwd']
+    db= config['DB']['db']
+
+    db=MySQLdb.connect(host,user,passwd,db)
+    cursor=db.cursor()
+    
+    return cursor
 
 def get_events(location):
 
-    db=MySQLdb.connect(host="10.0.0.205",port=3306,user="welkom",passwd="eventsopscherm",db="kvkaw")
-    cursor=db.cursor()
-    
+    cursor = DB_conn()
     events = None
     if location == "Antwerpen":
         cursor.execute("SELECT event, DATE_FORMAT(starttijd,'%H:%i'),locatie FROM welkomscherm WHERE vestiging = 'Antwerpen' AND datum = CURDATE() AND eindtijd >= CURTIME() ORDER BY starttijd ASC")
         events = cursor.fetchall()
         count = cursor.rowcount
-        #events = "Vooruitblik"
     if location == "Waasland":
         cursor.execute("SELECT event, DATE_FORMAT(starttijd,'%H:%i'),locatie FROM welkomscherm WHERE vestiging = 'Waasland' AND datum = CURDATE() AND eindtijd >= CURTIME() ORDER BY starttijd ASC")
         events = cursor.fetchall()
         count = cursor.rowcount
-        #events = "Nieuwsjaarsreceptie"
     return events, count
 
 
 def get_agenda():
 
-    db=MySQLdb.connect(host="10.0.0.205",port=3306,user="welkom",passwd="eventsopscherm",db="kvkaw")
-    cursor=db.cursor()
-    
+    cursor= DB_conn()
     agenda = None
-    cursor.execute("SELECT event, DATE_FORMAT(datum,'%e/%m') FROM kameragenda WHERE datum >= CURDATE() ORDER BY datum ASC LIMIT 3")
+    cursor.execute("SELECT event, DATE_FORMAT(datum,'%e/%m : '), type FROM kameragenda WHERE datum >= CURDATE() ORDER BY datum ASC LIMIT 3")
     agenda = cursor.fetchall()
     count = cursor.rowcount
     return agenda, count
+
