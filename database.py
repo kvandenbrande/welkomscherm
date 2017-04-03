c7598d7c6748df4f807a3ca168a88d8ff68a5547
kvandenbrande <kvandenbrande@gmail.com>
2017-04-03 11:02:41 +0200

    Update database.py

--- database.py
+++ database.py
@@ -39,7 +39,7 @@ def get_agenda():
 
     cursor= DB_conn()
     agenda = None
-    cursor.execute("SELECT event, DATE_FORMAT(datum,'%e/%m : '), type FROM kameragenda WHERE datum >= CURDATE() ORDER BY datum ASC LIMIT 3")
+    cursor.execute("SELECT event, DATE_FORMAT(datum,'%d/%m : '), type FROM kameragenda WHERE datum >= CURDATE() ORDER BY datum ASC LIMIT 3")
     agenda = cursor.fetchall()
     count = cursor.rowcount
     return agenda, count
