a6f6c5421445b41a15d88846a769ddd538c1f5e3
kvandenbrande <kvandenbrande@gmail.com>
2018-02-26 14:42:10 +0100

    Update welkomscherm.py

--- welkomscherm.py
+++ welkomscherm.py
@@ -8,10 +8,10 @@ import subprocess
 import json
 from subprocess import Popen
 from pygame.locals import *
-from background import load_background
+from colors import *
 from database import get_events
 from database import get_agenda
-from mpd import MPDClient
+##from mpd import MPDClient
 
 pygame.init()
 
@@ -32,18 +32,21 @@ try:
 except IOError:
     print "Error: online.tmp cannot be made."
 
-STARTUP = pygame.font.SysFont('monospace', 25)
-TEXT = pygame.font.SysFont('roboto', 35, bold=True)
-TITLE = pygame.font.SysFont('roboto', 50, bold=True)
-TITLE1 = pygame.font.SysFont('roboto', 75, bold=True)
+# media files
+LOCALEPATHIMAGE = '/home/pi/welkomscherm/img/'
+LOCALEPATHFONT = '/home/pi/welkomscherm/font/'
+MOVIE = '/home/pi/Desktop/jaaroverzicht.mp4'
+STREAM = "http://lb.zenfm.be/zenfm.mp3"
+
+STARTUP = pygame.font.Font(LOCALEPATHFONT + 'Rubik-Regular.ttf', 25)
+TEXT = pygame.font.Font(LOCALEPATHFONT + 'Rubik-Regular.ttf', 35, bold=True)
+TITLE = pygame.font.Font(LOCALEPATHFONT + 'Rubik-Regular.ttf', 40, bold=True)
+TITLE2 = pygame.font.Font(LOCALEPATHFONT + 'Rubik-Bold.ttf', 50, bold=True)
+TITLE1 = pygame.font.Font(LOCALEPATHFONT + 'Rubik-Bold.ttf', 75, bold=True)
 WHITE = (255, 255, 255)
 WAITTIME = 10  # default time to wait between images (in seconds)
 THICKNESS = 40
 
-# media files
-LOCALEPATH = '/home/pi/welkomscherm/img/'
-MOVIE = '/home/pi/Desktop/jaaroverzicht.mp4'
-STREAM = "http://lb.zenfm.be/zenfm.mp3"
 
 # set up the window, max screensize, fullscreen no frames
 
@@ -57,22 +60,18 @@ pygame.mouse.set_visible(False)
 
 L_STARTUP_X = 700 * x / 1920
 L_STARTUP_Y = 540 * y / 1080
-L_WELCOME_X = 150 * x / 1920
+L_WELCOME_X = 100 * x / 1920
 L_WELCOME_Y = 800 * y / 1080
-L_LOGO_X = 1350 * x / 1920
-L_LOGO_Y = 800 * y / 1080
-L_BORDER_AX = 100 * x / 1920
-L_BORDER_AY = 100 * y / 1080
-L_BORDER_BX = 1600 * x / 1920
-L_BORDER_CY = 900 * y / 1080
-L_EVENT_X = 150 * x / 1920
+L_LOGO_X = 100 * x / 1920
+L_LOGO_Y = 250 * y / 1080
+L_EVENT_X = 100 * x / 1920
 L_EVENTN_Y = 300 * y / 1080
 L_EVENTT_Y = 500 * y / 1080
 L_EVENTL_Y = 700 * y / 1080
-L_KA_TITEL_X = 150 * x / 1920
-L_KA_TITEL_Y = 150 * y / 1080
-L_KA_ED_X = 150 * x / 1920  # horizontaal afstand voor kameragenda datum
-L_KA_ET_X = 320 * x / 1920  # horizontaal afstand voor kameragenda event
+L_KA_TITEL_X = 100 * x / 1920
+L_KA_TITEL_Y = 100 * y / 1080
+L_KA_ED_X = 100 * x / 1920  # horizontaal afstand voor kameragenda datum
+L_KA_ET_X = 100 * x / 1920  # horizontaal afstand voor kameragenda event
 L_KA_3_E1_Y = 300 * y / 1080
 L_KA_3_E2_Y = 500 * y / 1080
 L_KA_3_E3_Y = 700 * y / 1080
@@ -85,6 +84,10 @@ L_KA_3_T3_Y = 775 * y / 1080
 L_KA_2_T1_Y = 575 * y / 1080
 L_KA_2_T2_Y = 875 * y / 1080
 L_KA_1_T1_Y = 575 * y / 1080
+L_RIGHT_X = 1252 * x / 1920       # positie rechtse afbeelding
+L_RIGHT_Y = 0                     # positie rechtse afbeelding
+L_SCALE_RIGHT_X = 668 * x / 1920  # schalen van rechtse afbeelding
+L_SCALE_RIGHT_Y = y               # schalen van rechtse afbeelding
 
 # startup screen
 
@@ -94,19 +97,19 @@ pygame.display.flip()
 
 # image config
 
-logo = pygame.image.load(LOCALEPATH + 'logo.png').convert_alpha()
-sms = pygame.image.load(LOCALEPATH + '8876.jpg')
-sms = sms.convert()
-sms = pygame.transform.scale(sms, max(modes))
+logo = pygame.image.load(LOCALEPATHIMAGE + 'VOKA_AW_LOGO_CMYK668.jpg')
+##sms = pygame.image.load(LOCALEPATHIMAGE + '8876.jpg')
+##sms = sms.convert()
+##sms = pygame.transform.scale(sms, max(modes))
 
-#event = pygame.image.load(LOCALEPATH +'EF.jpg')
+#event = pygame.image.load(LOCALEPATHIMAGE +'EF.jpg')
 #event = event.convert()
 #event = pygame.transform.scale(event, max(modes))
 
-partner1 = pygame.image.load(LOCALEPATH + 'PP_CORP.jpg')
+partner1 = pygame.image.load(LOCALEPATHIMAGE + 'PP_CORP.jpg')
 partner1 = partner1.convert()
 partner1 = pygame.transform.scale(partner1, max(modes))
-partner2 = pygame.image.load(LOCALEPATH + 'PP_BUSIACT.jpg')
+partner2 = pygame.image.load(LOCALEPATHIMAGE + 'PP_BUSIACT.jpg')
 partner2 = partner2.convert()
 partner2 = pygame.transform.scale(partner2, max(modes))
 
@@ -128,23 +131,20 @@ while True:
             pass
     # tekenen van welkom scherm ipv afbeelding
     
-    screen.fill(load_background())
-    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
-                     L_BORDER_AY), (L_BORDER_BX, L_BORDER_AY),
-                     THICKNESS)
-    pygame.draw.line(screen, WHITE, (L_BORDER_AX, L_BORDER_AY),
-                     (L_BORDER_AX, L_BORDER_CY), THICKNESS)
-    pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1,
-                     L_BORDER_CY), (L_BORDER_BX, L_BORDER_CY),
-                     THICKNESS)
-    pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2,
-                     L_BORDER_AY), (L_BORDER_BX - THICKNESS / 2,
-                     L_BORDER_CY), THICKNESS)
+    screen.fill(WHITE)
+    ORANGE = load_ORANGE()
+    GRAY = load_GRAY()
+    RECHTS = pygame.image.load(LOCALEPATHIMAGE + load_RECHTS())
+    RECHTS = RECHTS.convert()
+    RECHTS = pygame.transform.scale(RECHTS, (L_SCALE_RIGHT_X,L_SCALE_RIGHT_Y))
+    
 
     # render text
-    welcome = TITLE.render('VAN HARTE WELKOM', 5, WHITE)
+    welcome = TITLE1.render('VAN HARTE WELKOM', 5, GRAY)
     screen.blit(welcome, (L_WELCOME_X, L_WELCOME_Y))
 
+    screen.blit(RECHTS,(L_RIGHT_X, L_RIGHT_Y))
+    
     # add logo
     screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y, 128, 128))
     pygame.display.flip()
@@ -163,32 +163,37 @@ while True:
         pass
      
     while count > 0:
-        screen.fill(load_background())
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_AY),(L_BORDER_BX, L_BORDER_AY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX,L_BORDER_AY), (L_BORDER_AX,L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_CY),(L_BORDER_BX, L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2, L_BORDER_AY),(L_BORDER_BX - THICKNESS / 2,L_BORDER_CY), THICKNESS)
+        screen.fill(WHITE)
+        ORANGE = load_ORANGE()
+        GRAY = load_GRAY()
+        RECHTS = pygame.image.load(LOCALEPATHIMAGE + load_RECHTS())
+        RECHTS = RECHTS.convert()
+        RECHTS = pygame.transform.scale(RECHTS, (L_SCALE_RIGHT_X,L_SCALE_RIGHT_Y))
+
+        KA_TITEL = TITLE1.render('VANDAAG', 5, GRAY)
+        screen.blit(KA_TITEL, (L_KA_TITEL_X, L_KA_TITEL_Y))
 
         # render text
-        titelevent = TITLE.render(events[teller][0], 5, WHITE)
+        titelevent = TITLE.render(events[teller][0], 5, GRAY)
         screen.blit(titelevent, (L_EVENT_X, L_EVENTN_Y))
-        starttime = TITLE.render(events[teller][1], 5, WHITE)
+        starttime = TITLE2.render(events[teller][1], 5, ORANGE)
         screen.blit(starttime, (L_EVENT_X, L_EVENTT_Y))
-        location = TITLE.render(events[teller][2], 5, WHITE)
+        location = TITLE2.render(events[teller][2], 5, ORANGE)
         screen.blit(location, (L_EVENT_X, L_EVENTL_Y))
 
-        # add logo
-        screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y,128, 128))
+        # add image right
+        screen.blit(RECHTS,(L_RIGHT_X, L_RIGHT_Y))
+
         pygame.display.flip()
         time.sleep(WAITTIME)
         count = count - 1
         teller = teller + 1
 
-    # reclame 8876
-    if LOCATION_DETAIL != 'Auditorium':
-        screen.blit(sms, (0, 0))
-        pygame.display.flip()
-        time.sleep(WAITTIME)
+##    # reclame 8876
+##    if LOCATION_DETAIL != 'Auditorium':
+##        screen.blit(sms, (0, 0))
+##        pygame.display.flip()
+##        time.sleep(WAITTIME)
 
 ##    #reclame hoofdevent
 #    screen.blit(event,(0,0))
@@ -205,98 +210,107 @@ while True:
         
     
     if count == 3:
-        screen.fill(load_background())
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_AY),(L_BORDER_BX, L_BORDER_AY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX, L_BORDER_AY), (L_BORDER_AX,L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_CY),(L_BORDER_BX, L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2, L_BORDER_AY),(L_BORDER_BX - THICKNESS / 2,L_BORDER_CY), THICKNESS)
+        screen.fill(WHITE)
+        ORANGE = load_ORANGE()
+        GRAY = load_GRAY()
+        RECHTS = pygame.image.load(LOCALEPATHIMAGE + load_RECHTS())
+        RECHTS = RECHTS.convert()
+        RECHTS = pygame.transform.scale(RECHTS, (L_SCALE_RIGHT_X,L_SCALE_RIGHT_Y))
+
 
         # render text
-        KA_TITEL = TITLE1.render('Kameragenda', 5, WHITE)
+        KA_TITEL = TITLE1.render('AGENDA', 5, GRAY)
         screen.blit(KA_TITEL, (L_KA_TITEL_X, L_KA_TITEL_Y))
 
         # event1
-        agendadatum1 = TITLE.render(agenda[0][1], 5, WHITE)
+        agendadatum1 = TITLE2.render(agenda[0][1], 5, ORANGE)
         screen.blit(agendadatum1, (L_KA_ED_X, L_KA_3_E1_Y))
-        agendaitem1 = TITLE.render(agenda[0][0], 5, WHITE)
-        screen.blit(agendaitem1, (L_KA_ET_X, L_KA_3_E1_Y))
-        agendatype1 = TEXT.render(agenda[0][2], 5, WHITE)
-        screen.blit(agendatype1, (L_KA_ET_X, L_KA_3_T1_Y))
+        agendaitem1 = TITLE.render(agenda[0][0], 5, GRAY)
+        screen.blit(agendaitem1, (L_KA_ET_X, L_KA_3_T1_Y))
+        #agendatype1 = TEXT.render(agenda[0][2], 5, GRAY)
+        #screen.blit(agendatype1, (L_KA_ET_X, L_KA_3_T1_Y))
 
         # event2
-        agendadatum2 = TITLE.render(agenda[1][1], 5, WHITE)
+        agendadatum2 = TITLE2.render(agenda[1][1], 5, ORANGE)
         screen.blit(agendadatum2, (L_KA_ED_X, L_KA_3_E2_Y))
-        agendaitem2 = TITLE.render(agenda[1][0], 5, WHITE)
-        screen.blit(agendaitem2, (L_KA_ET_X, L_KA_3_E2_Y))
-        agendatype2 = TEXT.render(agenda[1][2], 5, WHITE)
-        screen.blit(agendatype2, (L_KA_ET_X, L_KA_3_T2_Y))
+        agendaitem2 = TITLE.render(agenda[1][0], 5, GRAY)
+        screen.blit(agendaitem2, (L_KA_ET_X, L_KA_3_T2_Y))
+        #agendatype2 = TEXT.render(agenda[1][2], 5, GRAY)
+        #screen.blit(agendatype2, (L_KA_ET_X, L_KA_3_T2_Y))
 
         # event3
-        agendadatum3 = TITLE.render(agenda[2][1], 5, WHITE)
+        agendadatum3 = TITLE2.render(agenda[2][1], 5, ORANGE)
         screen.blit(agendadatum3, (L_KA_ED_X, L_KA_3_E3_Y))
-        agendaitem3 = TITLE.render(agenda[2][0], 5, WHITE)
-        screen.blit(agendaitem3, (L_KA_ET_X, L_KA_3_E3_Y))
-        agendatype3 = TEXT.render(agenda[2][2], 5, WHITE)
-        screen.blit(agendatype3, (L_KA_ET_X, L_KA_3_T3_Y))
+        agendaitem3 = TITLE.render(agenda[2][0], 5, GRAY)
+        screen.blit(agendaitem3, (L_KA_ET_X, L_KA_3_T3_Y))
+        #agendatype3 = TEXT.render(agenda[2][2], 5, GRAY)
+        #screen.blit(agendatype3, (L_KA_ET_X, L_KA_3_T3_Y))
 
-        # add logo
-        screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y,128, 128))
+        # add image right
+        screen.blit(RECHTS,(L_RIGHT_X, L_RIGHT_Y))
+
         pygame.display.flip()
         time.sleep(WAITTIME)
 
     if count == 2:
-        screen.fill(load_background())
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_AY),(L_BORDER_BX, L_BORDER_AY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX, L_BORDER_AY), (L_BORDER_AX, L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_CY),(L_BORDER_BX, L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2, L_BORDER_AY),(L_BORDER_BX - THICKNESS / 2, L_BORDER_CY), THICKNESS)
+        screen.fill(WHITE)
+        ORANGE = load_ORANGE()
+        GRAY = load_GRAY()
+        RECHTS = pygame.image.load(LOCALEPATHIMAGE + load_RECHTS())
+        RECHTS = RECHTS.convert()
+        RECHTS = pygame.transform.scale(RECHTS, (L_SCALE_RIGHT_X,L_SCALE_RIGHT_Y))
+
 
         # render text
-        KA_TITEL = TITLE1.render('Kameragenda', 5, WHITE)
+        KA_TITEL = TITLE1.render('AGENDA', 5, GRAY)
         screen.blit(KA_TITEL, (L_KA_TITEL_X, L_KA_TITEL_Y))
 
         # event1
-        agendadatum1 = TITLE.render(agenda[0][1], 5, WHITE)
+        agendadatum1 = TITLE2.render(agenda[0][1], 5, ORANGE)
         screen.blit(agendadatum1, (L_KA_ED_X, L_KA_2_E1_Y))
-        agendaitem1 = TITLE.render(agenda[0][0], 5, WHITE)
-        screen.blit(agendaitem1, (L_KA_ET_X, L_KA_2_E1_Y))
-        agendatype1 = TEXT.render(agenda[0][2], 5, WHITE)
-        screen.blit(agendatype1, (L_KA_ET_X, L_KA_2_T1_Y))
+        agendaitem1 = TITLE.render(agenda[0][0], 5, GRAY)
+        screen.blit(agendaitem1, (L_KA_ET_X, L_KA_2_T1_Y))
+        #agendatype1 = TEXT.render(agenda[0][2], 5, GRAY)
+        #screen.blit(agendatype1, (L_KA_ET_X, L_KA_2_T1_Y))
 
         # event2
-        agendadatum2 = TITLE.render(agenda[1][1], 5, WHITE)
+        agendadatum2 = TITLE2.render(agenda[1][1], 5, ORANGE)
         screen.blit(agendadatum2, (L_KA_ED_X, L_KA_2_E2_Y))
-        agendaitem2 = TITLE.render(agenda[1][0], 5, WHITE)
-        screen.blit(agendaitem2, (L_KA_ET_X, L_KA_2_E2_Y))
-        agendatype2 = TEXT.render(agenda[1][2], 5, WHITE)
-        screen.blit(agendatype2, (L_KA_ET_X, L_KA_2_T2_Y))
+        agendaitem2 = TITLE.render(agenda[1][0], 5, GRAY)
+        screen.blit(agendaitem2, (L_KA_ET_X, L_KA_2_T2_Y))
+        #agendatype2 = TEXT.render(agenda[1][2], 5, GRAY)
+        #screen.blit(agendatype2, (L_KA_ET_X, L_KA_2_T2_Y))
 
-        # add logo
-        screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y,128, 128))
+        # add image right
+        screen.blit(RECHTS,(L_RIGHT_X, L_RIGHT_Y))
+        
         pygame.display.flip()
         time.sleep(WAITTIME)
 
     if count == 1:
-        screen.fill(load_background())
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_AY),(L_BORDER_BX, L_BORDER_AY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX, L_BORDER_AY), (L_BORDER_AX, L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_AX - THICKNESS / 2 + 1, L_BORDER_CY),(L_BORDER_BX, L_BORDER_CY), THICKNESS)
-        pygame.draw.line(screen, WHITE, (L_BORDER_BX - THICKNESS / 2, L_BORDER_AY),(L_BORDER_BX - THICKNESS / 2, L_BORDER_CY), THICKNESS)
+        screen.fill(WHITE)
+        ORANGE = load_ORANGE()
+        GRAY = load_GRAY()
+        RECHTS = pygame.image.load(LOCALEPATHIMAGE + load_RECHTS())
+        RECHTS = RECHTS.convert()
+        RECHTS = pygame.transform.scale(RECHTS, (L_SCALE_RIGHT_X,L_SCALE_RIGHT_Y))
+
 
         # render text
-        KA_TITEL = TITLE1.render('Kameragenda', 5, WHITE)
+        KA_TITEL = TITLE1.render('AGENDA', 5, GRAY)
         screen.blit(KA_TITEL, (L_KA_TITEL_X, L_KA_TITEL_Y))
 
         # event1
-        agendadatum1 = TITLE.render(agenda[0][1], 5, WHITE)
+        agendadatum1 = TITLE2.render(agenda[0][1], 5, ORANGE)
         screen.blit(agendadatum1, (L_KA_ED_X, L_KA_1_E1_Y))
-        agendaitem1 = TITLE.render(agenda[0][0], 5, WHITE)
-        screen.blit(agendaitem1, (L_KA_ET_X, L_KA_1_E1_Y))
-        agendatype1 = TEXT.render(agenda[0][2], 5, WHITE)
-        screen.blit(agendatype1, (L_KA_ET_X, L_KA_1_T1_Y))
+        agendaitem1 = TITLE.render(agenda[0][0], 5, GRAY)
+        screen.blit(agendaitem1, (L_KA_ET_X, L_KA_1_T1_Y))
+        #agendatype1 = TEXT.render(agenda[0][2], 5, GRAY)
+        #screen.blit(agendatype1, (L_KA_ET_X, L_KA_1_T1_Y))
+        
+        # add image right
+        screen.blit(RECHTS,(L_RIGHT_X, L_RIGHT_Y))
 
-        # add logo
-        screen.blit(logo, pygame.rect.Rect(L_LOGO_X, L_LOGO_Y,128, 128))
         pygame.display.flip()
         time.sleep(WAITTIME)
 
@@ -332,3 +346,4 @@ while True:
                 print "Error: movie.tmp cannot be made."
 
        
+
