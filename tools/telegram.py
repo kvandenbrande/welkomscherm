7ee0c364b2c0d0309564087eb7bb495c7c637f78
kvandenbrande <kvandenbrande@gmail.com>
2018-12-04 12:50:11 +0100

    Update telegram.py

--- tools/telegram.py
+++ tools/telegram.py
@@ -1,12 +1,11 @@
 import os, json
 
 def send_telegram(MESSAGE):
-
-try:
-    with open('/home/pi/Desktop/welkomscherm_conf.json', 'r') as f:
+    try:
+        with open('/home/pi/Desktop/welkomscherm_conf.json', 'r') as f:
             config = json.load(f)
     except ValueError:
-        print 'Error: Decoding config has failed'
+        print ('Error: Decoding config has failed')
 
     TELEGRAM_TOKEN = config['TELEGRAM']['token']
     TELEGRAM_CHAT_ID = config['TELEGRAM']['chatID']
